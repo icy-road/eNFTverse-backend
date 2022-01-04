59921b4d0a011f47c88c9888ff7597f7fa08be6a
Narcis <narcisc2007@gmail.com>
2022-01-04 23:31:57 +0200

    fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* eslint-disable global-require */
 
+console.log('Node version: ', process.version);
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 
