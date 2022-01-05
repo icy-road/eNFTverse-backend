958f826a436975ebaa9de4e70dd6e880118c1a1e
Narcis <narcisc2007@gmail.com>
2022-01-05 09:04:15 +0200

    fix

--- src/app.js
+++ src/app.js
@@ -1,25 +1,23 @@
-// const Koa = require('koa');
-// const bodyParser = require('koa-bodyparser');
-// const routes = require('./routes');
-// const { mongooseInstance } = require('./models');
-// const errorHandler = require('./common/errorHandler');
-//
-// const app = new Koa();
-//
-// app.context.mongooseInstance = mongooseInstance;
-//
-// app.use(bodyParser({
-//     enableTypes: ['json', 'form'],
-//     onerror: (err) => { // ignore json parse error
-//         debug('body parse error', err);
-//     },
-// }));
-// app.use(errorHandler);
-// app.use(routes);
-//
-//
-// app.listen(3000, () => { //todo: from config
-//     console.log(`Listening to ${3000}`);
-// });
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const routes = require('./routes');
+const { mongooseInstance } = require('./models');
+const errorHandler = require('./common/errorHandler');
 
-console.log('Node version: ', process.version);
+const app = new Koa();
+
+app.context.mongooseInstance = mongooseInstance;
+
+app.use(bodyParser({
+    enableTypes: ['json', 'form'],
+    onerror: (err) => { // ignore json parse error
+        debug('body parse error', err);
+    },
+}));
+app.use(errorHandler);
+app.use(routes);
+
+
+app.listen(3000, () => { //todo: from config
+    console.log(`Listening to ${3000}`);
+});
