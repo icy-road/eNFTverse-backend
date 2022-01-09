b93d7828646f33aca4e80ba052bfa98ecb8f2ced
narcis <narcis@creamcapital.io>
2022-01-09 23:32:30 +0200

    port

--- src/app.js
+++ src/app.js
@@ -18,6 +18,6 @@ app.use(errorHandler);
 app.use(routes);
 
 
-app.listen(8008 , () => { //todo: from config
+app.listen(8080 , () => { //todo: from config
     console.log(`Listening to ${8008 }`);
 });
